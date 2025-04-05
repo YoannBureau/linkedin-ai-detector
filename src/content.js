e290316b7c31198ca16f5f2110da16d6f7886c8c
Yoann Bureau <ybureau@gmail.com>
2025-04-05 09:48:48 +0200

    Enhance LinkedIn post interaction by adding icon click functionality to gptzero and refining text extraction

--- src/content.js
+++ src/content.js
@@ -1,15 +1,22 @@
 const posts = document.querySelectorAll('.feed-shared-inline-show-more-text');
 posts.forEach((post) => {
-    // Replace <br> tags with line breaks and extract plain text
-    const htmlContent = post.innerHTML.replace(/<br\s*\/?>/gi, '\n'); // Replace <br> with \n
-    const plainText = htmlContent.replace(/<\/?[^>]+(>|$)/g, '').trim(); // Remove remaining HTML tags
-    console.log('Found one post!', plainText);
-    
-    // Create a new image element
-    const img = document.createElement('img');
-    img.src = imgUrl = chrome.runtime.getURL('icon.png');
-    img.classList.add('lpaid-icon');
+    // Replace <br> tags with line breaks, extract plain text and remove "...more" text
+    const htmlContent = post.innerHTML.replace(/<br\s*\/?>/gi, '%0A');
+    let plainText = htmlContent.replace(/<\/?[^>]+(>|$)/g, '').trim();
+    plainText = plainText.replace(/\.\.\.more$/, '').trim();
 
-    // Insert the image at the beginning of the post content
-    post.prepend(img);
+    const extensionImg = document.createElement('img');
+    extensionImg.src = imgUrl = chrome.runtime.getURL('icon.png');
+    extensionImg.alt = 'AI Post Detector';
+    extensionImg.classList.add('lpaid-icon');
+
+    extensionImg.addEventListener('click', () => {
+        handleIconClick(plainText);
+    });
+
+    post.prepend(extensionImg);
 });
+
+function handleIconClick(plainText) {
+    window.open('https://app.gptzero.me/?scanType=basic&source=landing&triggerScan=true&text='+plainText, '_blank');
+}
